ec266ef6c53f167f2a50e57b878a4dcb1d8a10d0
fenil09 <backupsystem9909@gmail.com>
2023-11-16 20:37:15 -0800

    Splash screen integrated.

--- app/src/main/java/com/base/clean_architecture_newapp/MainActivity.kt
+++ app/src/main/java/com/base/clean_architecture_newapp/MainActivity.kt
@@ -10,14 +10,15 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.base.clean_architecture_newapp.ui.theme.Clean_Architecture_NewAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             Clean_Architecture_NewAppTheme {
-
             }
         }
     }
