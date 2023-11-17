4615d2dada3e47edcf28068aa15b10e8fac397b3
fenil09 <backupsystem9909@gmail.com>
2023-11-16 19:54:55 -0800

    Initial commit

---
+++ app/src/main/java/com/base/clean_architecture_newapp/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.base.clean_architecture_newapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.base.clean_architecture_newapp.ui.theme.Clean_Architecture_NewAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Clean_Architecture_NewAppTheme {
+
+            }
+        }
+    }
+}
+
